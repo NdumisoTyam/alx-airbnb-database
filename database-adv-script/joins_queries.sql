907532ef45491eff56046bf7b72b60f1eecaae63
NdumisoTyam <Ndumisotyam1@gmail.com>
2025-07-06 21:48:14 +0200

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,1 +1,33 @@
+SELECT 
+    bookings.booking_id,
+    bookings.property_id,
+    bookings.check_in_date,
+    bookings.check_out_date,
+    bookings.total_price,
+    bookings.status,
+    users.user_id,
+    users.name,
+    users.email
+FROM bookings
+INNER JOIN users ON bookings.user_id = users.user_id;
 
+SELECT 
+    properties.property_id,
+    properties.title,
+    properties.city,
+    reviews.review_id,
+    reviews.rating,
+    reviews.comment
+FROM properties
+LEFT JOIN reviews ON properties.property_id = reviews.property_id;
+
+SELECT 
+    users.user_id,
+    users.name,
+    users.email,
+    bookings.booking_id,
+    bookings.property_id,
+    bookings.check_in_date,
+    bookings.total_price
+FROM users
+FULL OUTER JOIN bookings ON users.user_id = bookings.user_id;

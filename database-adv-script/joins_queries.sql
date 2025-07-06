9f9152a80db3ff6726c766aca586ba9c3c1c3766
NdumisoTyam <Ndumisotyam1@gmail.com>
2025-07-06 21:56:22 +0200

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -23,6 +23,15 @@ FROM properties
 LEFT JOIN reviews ON properties.property_id = reviews.property_id
 ORDER BY properties.property_id;
 
+-- Using FULL OUTER JOIN to retrieve all users and all bookings,
+-- even if the user has no booking or a booking is not linked to a user.
+-- NOTE: MySQL does not support FULL OUTER JOIN directly,
+-- so we simulate it using LEFT JOIN + UNION
+
+-- Equivalent to:
+-- SELECT * FROM users
+-- FULL OUTER JOIN bookings ON users.user_id = bookings.user_id;
+
 (
   SELECT 
     users.user_id,
@@ -49,4 +58,5 @@ UNION
   LEFT JOIN users ON users.user_id = bookings.user_id
 )
 ORDER BY user_id;
+
 

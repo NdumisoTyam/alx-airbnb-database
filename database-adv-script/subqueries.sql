368402f546ba119696f0388295ae4189372e8754
NdumisoTyam <Ndumisotyam1@gmail.com>
2025-07-06 21:57:51 +0200

    Create subqueries.sql

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,16 @@
+SELECT *
+FROM properties
+WHERE property_id IN (
+  SELECT property_id
+  FROM reviews
+  GROUP BY property_id
+  HAVING AVG(rating) > 4.0
+);
+
+SELECT user_id, name, email
+FROM users u
+WHERE (
+  SELECT COUNT(*)
+  FROM bookings b
+  WHERE b.user_id = u.user_id
+) > 3;

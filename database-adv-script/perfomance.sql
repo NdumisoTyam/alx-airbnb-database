3858eb2ccd5adb4a0d2ebd82fd4da43119c70582
NdumisoTyam <Ndumisotyam1@gmail.com>
2025-07-06 22:27:37 +0200

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,5 +1,3 @@
--- performance.sql
-
 SELECT 
   b.booking_id,
   b.booking_date,
@@ -39,15 +37,21 @@ SELECT
   b.booking_id,
   b.booking_date,
   u.user_id,
-  u.name,
+  u.name AS user_name,
+  u.email AS user_email,
   p.property_id,
-  p.title,
-  pay.amount
+  p.title AS property_title,
+  p.location,
+  pay.payment_id,
+  pay.amount,
+  pay.payment_date
 FROM bookings b
 JOIN users u ON b.user_id = u.user_id
 JOIN properties p ON b.property_id = p.property_id
 JOIN payments pay ON b.booking_id = pay.booking_id
-WHERE b.booking_date >= CURRENT_DATE - INTERVAL '1 year';
+WHERE b.booking_date >= CURRENT_DATE - INTERVAL '1 year'
+  AND pay.amount > 0;
+
 
 CREATE INDEX idx_bookings_user_id ON bookings(user_id);
 CREATE INDEX idx_bookings_property_id ON bookings(property_id);
